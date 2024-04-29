2fd2ac05217ff882fc1839bba50f1b144923ad46
goutham4331 <goutham4331@gmail.com>
2024-04-28 22:21:46 -0500

    hbj

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom"
 import Avatar from "./Avatar"
+import { useEffect, useState } from "react"
 
 const Appbar = () => {
+    const [visible,setVisible ] = useState(false)
+    useEffect(()=>{
+        if (localStorage.getItem("jwt")){
+            setVisible(true)
+        }
+      },[])
+    
     return (
         <div className="flex justify-between p-4 border-b-2 mb-4">
             <Link to="/blogs"><div>Medium</div></Link>
             <div className="flex justify-between">
-            <Link to="/writeblog" >Write</Link>
+            {visible&&<Link to="/writeblog" >Write</Link>}
             <Avatar authorName="trash hero" />
             </div>
         </div>

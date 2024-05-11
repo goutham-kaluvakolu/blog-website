bb61882ae76841c50dbbef845fecacd7b6fa8b5d
goutham4331 <goutham4331@gmail.com>
2024-05-11 13:24:40 -0500

    added bookmark icon

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -1,24 +1,24 @@
 import { Link } from "react-router-dom"
 import Avatar from "./Avatar"
+import Bookmark from "./Bookmark"
 
 type blogcardProps = {
-    "id":string
+    "id": string
     "authorName": string,
     "content": string,
     "title": string,
-    "authorId"?:string
+    "authorId"?: string
 }
 
 
 
-const BlogCard = ({ id ,authorName, content, title,authorId }: blogcardProps) => {
+const BlogCard = ({ id, authorName, content, title, authorId }: blogcardProps) => {
     return (
-        <div className="border-b-2 mb-2 w-2/3 h-28 ">
-            <Link to={`/blog/${id}`}>
+        <div className="border-b-2 w-2/3 h-38 mt-6 ">
             {/* header */}
             <div></div>
             <div className="flex items-center ">
-                <Avatar authorName={authorName}/>
+                <Avatar authorName={authorName} />
                 {/* name */}
                 <Link to={`/Author/${authorId}`} className="mr-2">{authorName}</Link>
                 {/* dot */}
@@ -26,20 +26,23 @@ const BlogCard = ({ id ,authorName, content, title,authorId }: blogcardProps) =>
                 {/* date */}
                 <span className="text-stone-500">3 dec 2224</span>
             </div>
-
-            {/* title */}
-            <div className="font-bold text-3xl">
-                {title}
-            </div>
-            {/* few lines */}
-            <article className="text-ellipsis overflow-hidden text-slate-600">
-                {content}
-            </article>
-            {/* footer */}
-            <div>
-                {/* icons */}
-            </div>
+            <Link to={`/blog/${id}`}>
+                {/* title */}
+                <div className="font-bold text-3xl">
+                    {title}
+                </div>
+                {/* few lines */}
+                <article className="text-ellipsis overflow-hidden text-slate-600 text-base">
+                    {content}
+                </article>
             </Link>
+            {/* footer */}
+            <div className="flex flex-row-reverse mb-6">
+                {/* icons */}
+                <Bookmark />
+
+            </div>
+
         </div>
     )
 }
